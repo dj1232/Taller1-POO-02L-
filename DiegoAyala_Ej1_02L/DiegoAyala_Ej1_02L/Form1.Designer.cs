4ada1534e74ce4f01d2271be39902c3a42466315
Diego <diegoayala1232@hotmail.com>
2021-09-02 18:02:48 -0600

    Ejercicio 1

---
+++ DiegoAyala_Ej1_02L/DiegoAyala_Ej1_02L/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace DiegoAyala_Ej1_02L
+{
+    partial class pantalla_1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.txt_a = new System.Windows.Forms.TextBox();
+            this.txt_b = new System.Windows.Forms.TextBox();
+            this.txt_c = new System.Windows.Forms.TextBox();
+            this.lb_a = new System.Windows.Forms.Label();
+            this.lb_b = new System.Windows.Forms.Label();
+            this.lb_c = new System.Windows.Forms.Label();
+            this.lbt_x1 = new System.Windows.Forms.Label();
+            this.lbr_x1 = new System.Windows.Forms.Label();
+            this.lbt_x2 = new System.Windows.Forms.Label();
+            this.lbr_x2 = new System.Windows.Forms.Label();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(106, 406);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(94, 29);
+            this.btn_calcular.TabIndex = 0;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Location = new System.Drawing.Point(316, 406);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(94, 29);
+            this.btn_limpiar.TabIndex = 1;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click_1);
+            // 
+            // txt_a
+            // 
+            this.txt_a.Location = new System.Drawing.Point(316, 78);
+            this.txt_a.Name = "txt_a";
+            this.txt_a.Size = new System.Drawing.Size(125, 27);
+            this.txt_a.TabIndex = 2;
+            // 
+            // txt_b
+            // 
+            this.txt_b.Location = new System.Drawing.Point(316, 170);
+            this.txt_b.Name = "txt_b";
+            this.txt_b.Size = new System.Drawing.Size(125, 27);
+            this.txt_b.TabIndex = 3;
+            // 
+            // txt_c
+            // 
+            this.txt_c.Location = new System.Drawing.Point(316, 280);
+            this.txt_c.Name = "txt_c";
+            this.txt_c.Size = new System.Drawing.Size(125, 27);
+            this.txt_c.TabIndex = 4;
+            // 
+            // lb_a
+            // 
+            this.lb_a.AutoSize = true;
+            this.lb_a.Location = new System.Drawing.Point(106, 78);
+            this.lb_a.Name = "lb_a";
+            this.lb_a.Size = new System.Drawing.Size(24, 20);
+            this.lb_a.TabIndex = 5;
+            this.lb_a.Text = "a :";
+            // 
+            // lb_b
+            // 
+            this.lb_b.AutoSize = true;
+            this.lb_b.Location = new System.Drawing.Point(106, 170);
+            this.lb_b.Name = "lb_b";
+            this.lb_b.Size = new System.Drawing.Size(25, 20);
+            this.lb_b.TabIndex = 6;
+            this.lb_b.Text = "b :";
+            // 
+            // lb_c
+            // 
+            this.lb_c.AutoSize = true;
+            this.lb_c.Location = new System.Drawing.Point(106, 280);
+            this.lb_c.Name = "lb_c";
+            this.lb_c.Size = new System.Drawing.Size(23, 20);
+            this.lb_c.TabIndex = 7;
+            this.lb_c.Text = "c :";
+            // 
+            // lbt_x1
+            // 
+            this.lbt_x1.AutoSize = true;
+            this.lbt_x1.Location = new System.Drawing.Point(585, 146);
+            this.lbt_x1.Name = "lbt_x1";
+            this.lbt_x1.Size = new System.Drawing.Size(31, 20);
+            this.lbt_x1.TabIndex = 8;
+            this.lbt_x1.Text = "x1 :";
+            // 
+            // lbr_x1
+            // 
+            this.lbr_x1.AutoSize = true;
+            this.lbr_x1.Location = new System.Drawing.Point(670, 146);
+            this.lbr_x1.Name = "lbr_x1";
+            this.lbr_x1.Size = new System.Drawing.Size(0, 20);
+            this.lbr_x1.TabIndex = 9;
+            // 
+            // lbt_x2
+            // 
+            this.lbt_x2.AutoSize = true;
+            this.lbt_x2.Location = new System.Drawing.Point(585, 245);
+            this.lbt_x2.Name = "lbt_x2";
+            this.lbt_x2.Size = new System.Drawing.Size(31, 20);
+            this.lbt_x2.TabIndex = 10;
+            this.lbt_x2.Text = "x2 :";
+            // 
+            // lbr_x2
+            // 
+            this.lbr_x2.AutoSize = true;
+            this.lbr_x2.Location = new System.Drawing.Point(670, 245);
+            this.lbr_x2.Name = "lbr_x2";
+            this.lbr_x2.Size = new System.Drawing.Size(0, 20);
+            this.lbr_x2.TabIndex = 11;
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(540, 406);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(94, 29);
+            this.btn_salir.TabIndex = 12;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // pantalla_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 620);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.lbr_x2);
+            this.Controls.Add(this.lbt_x2);
+            this.Controls.Add(this.lbr_x1);
+            this.Controls.Add(this.lbt_x1);
+            this.Controls.Add(this.lb_c);
+            this.Controls.Add(this.lb_b);
+            this.Controls.Add(this.lb_a);
+            this.Controls.Add(this.txt_c);
+            this.Controls.Add(this.txt_b);
+            this.Controls.Add(this.txt_a);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_calcular);
+            this.Name = "pantalla_1";
+            this.Text = "Formula General";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.TextBox txt_a;
+        private System.Windows.Forms.TextBox txt_b;
+        private System.Windows.Forms.TextBox txt_c;
+        private System.Windows.Forms.Label lb_a;
+        private System.Windows.Forms.Label lb_b;
+        private System.Windows.Forms.Label lb_c;
+        private System.Windows.Forms.Label lbt_x1;
+        private System.Windows.Forms.Label lbr_x1;
+        private System.Windows.Forms.Label lbt_x2;
+        private System.Windows.Forms.Label lbr_x2;
+        private System.Windows.Forms.Button btn_salir;
+    }
+}
+
---
+++ DiegoAyala_Ej1_02L/DiegoAyala_Ej1_02L/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiegoAyala_Ej1_02L
+{
+    public partial class pantalla_1 : Form
+    {
+        public pantalla_1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            double a, b, c, x1, x2;
+
+            //obtencion de datos
+            a = Convert.ToDouble(txt_a.Text);
+            b = Convert.ToDouble(txt_b.Text);
+            c = Convert.ToDouble(txt_c.Text);
+
+            //operaciones
+            x1 = (-1 * b + Math.Sqrt(Math.Pow(b, 2) - (4 * a * c))) / (2 * a);
+            x2 = (-1 * b - Math.Sqrt(Math.Pow(b, 2) - (4 * a * c))) / (2 * a);
+
+            //impresion de resultados
+            lbr_x1.Text = Convert.ToString(x1);
+            lbr_x2.Text = Convert.ToString(x2);
+
+        }
+
+     
+        private void btn_limpiar_Click_1(object sender, EventArgs e)
+        {
+            //limpiando campos...
+            txt_a.Clear();
+            txt_b.Clear();
+            txt_c.Clear();
+            lbr_x1.Text = "";
+            lbr_x2.Text = "";
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();//termina la aplicacion
+        }
+    }
+}
